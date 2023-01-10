5de7c6daad2163689a60608df1f8f2c07bf396ee
Brett de Bear <bkdebear@gmail.com>
2023-01-10 10:46:56 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10) 
+
+let num3 = 16
+let num4 = 4
+const mod = num4 % num3;
+
+let max = Math.max(5, 10, 15, 20);
